f8554807595064fbcf3be3e14b304a232a870705
jatajatat <jata7777@icloud.com>
2022-04-12 15:58:12 -0400

    clean up

--- qr_code_reader.py
+++ qr_code_reader.py
@@ -1,7 +1,5 @@
-import math
 import cv2 as cv
 import numpy as np
-from scipy.spatial.transform import Rotation as R
 import transforms3d.euler as eul
 
 def read_camera_parameters(filepath = 'intrinsicParameters/'):
